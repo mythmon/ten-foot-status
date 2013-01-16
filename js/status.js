74b82c83e801b9d65d876af913ab082b1a1db8f3
Mike Cooper <mythmon@gmail.com>
2013-01-15 17:39:55 -0800

    Initial implementation.

---
+++ js/status.js
@@ -0,0 +1,72 @@
+/* jslint multistr */
+var messages;
+var waitCount = 2;
+
+var status_template =
+'<li class="status {status}">' +
+  '<p>{body}</p>' +
+  '<time datetime={created_on}>{created_on_disp}</time>' +
+'</li>';
+
+var header_template =
+'<header class={status}>' +
+  '<h1>Github Status</h1>' +
+  '<h2>{status_disp}</h2>' +
+  '<span>As of {created_on_disp}</span>' +
+'</header>';
+
+function formatDate(date) {
+  var now = new Date();
+  var date_disp;
+
+  date_disp = date.getHours() + ':' + date.getMinutes();
+  date_disp += ' - ';
+
+  if (now - date < 24 * 60 * 60 * 1000) {
+    date_disp += 'Today';
+  } else if (now - date < 48 * 60 * 60 * 1000) {
+    date_disp += 'Yesterday';
+  } else {
+    date_disp += date.getFullYear() + '-';
+    var month = date.getMonth() + 1;
+    if (month < 10) {
+      date_disp += '0';
+    }
+    date_disp += month + '-' + date.getDate();
+  }
+
+  return date_disp;
+}
+
+function ready() {
+  console.log('ready ' + waitCount);
+  if (--waitCount) return;
+
+  var $messagesFragment = $('<ul/>', {'class': 'messages'});
+  _.each(messages, function(msg) {
+    msg.created_on_disp = formatDate(new Date(msg.created_on));
+    $messagesFragment.append($(status_template.format(msg)));
+  });
+
+  var latest = messages[0];
+  if (latest.status === 'good') {
+    latest.status_disp = 'Battle stations fully operational';
+  } else if (latest.status === 'minor') {
+    latest.status_disp = 'Minor disruption';
+  } else if (latest.status === 'major') {
+    latest.status_disp = 'Major disruption';
+  } else {
+    latest.status_disp = 'Unknown';
+  }
+
+  $('#wrap')
+    .html(header_template.format(latest))
+    .append($messagesFragment);
+}
+
+$.getJSON('https://status.github.com/api/messages.json?callback=?', {}, function(msgs) {
+    messages = msgs;
+    ready();
+  });
+
+$(ready);
